9c3ccb8823f08ce46e730a772b37fe7c2ea13fd0
Chris Mosier <chrismosier13@gmail.com>
2018-01-03 16:13:39 -0800

    create database for future use and add cdns for icons

---
+++ src/config/database.js
@@ -0,0 +1,65 @@
+const database = [
+    {
+    name: "Alien Sweeper",
+    img: "https://i.imgur.com/H8FWCP3.png",
+    description: "An alien themed minesweeper game that has multiple difficulties, allows placement of flags, and has media inputs. It was built using Javascript, jQuery, HTML5, and CSS3.",
+    keyWords: [
+        {keyword: "Javascript", icon: "devicon-javascript-plain"},
+        {keyword: "jQuery", icon: "devicon-jquery-plain colored"},
+        {keyword: "HTML5", icon: "devicon-html5-plain colored"},
+        {keyword: "CSS3", icon: "devicon-css3-plain colored"}
+    ],
+    liveURL: "https://chrism13.github.io/MineSweeper/",
+    repoURL: "https://github.com/ChrisM13/MineSweeper"
+},
+    {
+    name: "Lets Work",
+    img: "https://i.imgur.com/H8FWCP3.png",
+    description: "Lets Work is an app that allows an employer/manager veiw and update employee                  shifts for their own record. It was built using the MERN (MongoDB, Express, React, and Node) Stack.",
+    keyWords: [
+        {keyword: "React.js", icon: "devicon-react-original colored"},
+        {keyword: "Javascript", icon: "devicon-javascript-plain"},
+        {keyword: "MongoDB", icon: "devicon-mongodb-plain colored"},
+        {keyword: "Express", icon: "devicon-express-original colored"},
+        {keyword: "Node.js", icon: "devicon-nodejs-plain colored"},
+        {keyword: "HTML5", icon: "devicon-html5-plain colored"},
+        {keyword: "CSS3", icon: "devicon-css3-plain colored"},
+        {keyword: "Materialize", icon: ""}
+    ],
+    liveURL: "https://lets-work.herokuapp.com/",
+    repoURL: "https://github.com/ChrisM13/Lets-Work"
+},
+    {
+    name: "Divide and Costco",
+    img: "https://i.imgur.com/H8FWCP3.png",
+    description: "Divide and Costco is an app that allows Costco shoppers to connect for savings. It allows two customers to connect shopping lists to split large quantity products. That way they reduce waste while also saving money on the items they need. It was built using the MEN (MongoDB, Express,and Node) Stack.",
+    keyWords: [
+        {keyword: "Node.js", icon: "devicon-nodejs-plain colored"},
+        {keyword: "Javascript", icon: "devicon-javascript-plain"},
+        {keyword: "Express", icon: "devicon-express-original colored"},
+        {keyword: "MongoDB", icon: "devicon-mongodb-plain colored"},
+        {keyword: "HTML5", icon: "devicon-html5-plain colored"},
+        {keyword: "CSS3", icon: "devicon-css3-plain colored"},
+        {keyword: "Materialize", icon: ""},
+        {keyword: "GoogleMapsAPI", icon: ""},
+        {keyword: "YelpAPI", icon: ""}
+    ],
+    liveURL: "https://divide-and-costco.herokuapp.com/",
+    repoURL: "https://github.com/ChrisM13/MineSweeper"
+},
+    {
+    name: "Whatcha Think",
+    img: "https://i.imgur.com/H8FWCP3.png",
+    description: "A survey app that allows an admin to create custom surveys and view all answers submitted by non admin users. It was built using Ruby, Ruby on Rails, HTML5 and CSS3.",
+    keyWords: [
+        {keyword: "Ruby on Rails", icon: "devicon-rails-plain colored"},
+        {keyword: "Ruby", icon: "devicon-ruby-plain colored"},
+        {keyword: "HTML5", icon: "devicon-html5-plain colored"},
+        {keyword: "CSS3", icon: "devicon-css3-plain colored"}
+    ],
+    liveURL: "https://watchathink.herokuapp.com/",
+    repoURL: "https://github.com/ChrisM13/Whatcha-Think"
+}
+]
+
+export default database
