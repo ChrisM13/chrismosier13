a551a5279aad0d9e0de4f8bbbf616bf02a9ec445
Chris Mosier <chrismosier13@gmail.com>
2018-01-05 15:00:21 -0800

    add basename to Router in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Router > 
+        <Router basename={process.env.PUBLIC_URL} > 
           <div>
             <NavBar />
               <Switch>
